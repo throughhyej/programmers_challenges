944b719c032fe7cc4912eeacef2682efb825f397
throughhyej <hyejeyh@gmail.com>
2020-09-05 21:25:20 +0900

    Scanner, toCharArray(), String.valueOf(), 정규식, StringBuffer, StringBuilder, CharSequence

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
     /* https://programmers.co.kr/learn/challenges */
     public static void main(String[] args) {
         // write your code here
-        
+        BlockedPhoneNum_2 runCode = new BlockedPhoneNum_2();
+        runCode.run();
     }
 }
