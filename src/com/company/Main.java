8efda80d33df760ca4efd04703dff65e7ad1bfe6
throughhyej <hyejeyh@gmail.com>
2020-09-05 10:21:47 +0900

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    /* https://programmers.co.kr/learn/challenges */
+    public static void main(String[] args) {
+        // write your code here
+        
+    }
+}
