0318444147f5a2efa707b9fc472ade4c74e78f54
throughhyej <hyejeyh@gmail.com>
2021-01-24 13:13:47 +0900

    정수 내림차순 배치하기: 어...려....워.....

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
     /* https://programmers.co.kr/learn/challenges */
     public static void main(String[] args) {
         // write your code here
-        EvenOdd_7 runCode = new EvenOdd_7();
-        runCode.run(-3);
+        Desc_8 runCode = new Desc_8();
+        runCode.run(199201938);
     }
 }
