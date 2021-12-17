31419fcc0c3014430da3f3c10b26b6347ea2ca72
throughhyej <hyejeyh@gmail.com>
2021-12-17 23:53:24 +0900

    [프로그래머스] 키패드 누르기

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,10 @@
 package com.company;
 
-import com.company.level1.Convert;
-import com.company.level1.Filtering;
-import com.company.level1.Lotto;
-import com.company.level1.ValidationId;
+import com.company.level1.*;
 
 public class Main {
     /* https://programmers.co.kr/learn/challenges */
     public static void main(String[] args) {
-        // write your code here
 //        Desc_8 runCode = new Desc_8();
 //        runCode.run(199201938);
 
@@ -21,7 +17,10 @@ public class Main {
 
 //        new Convert().converToNum("23four5six7");
 
-        int[] intArry = new int[] {1,2,3,4,6,7,8,0};
-        new Filtering().filter(intArry);
+//        int[] intArry = new int[] {1,2,3,4,6,7,8,0};
+//        new Filtering().filter(intArry);
+
+        int[] intArry = new int[] {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        new Keypad().pressKeypad(intArry, "right");
     }
 }
---
+++ src/com/company/level1/Keypad.java
@@ -0,0 +1,74 @@
+package com.company.level1;
+
+import java.util.Arrays;
+
+public class Keypad {
+    /**
+     * https://programmers.co.kr/learn/courses/30/lessons/67256
+     * 키패드 누르기
+     *
+     * 중복 소스 함수화
+     **/
+    public String pressKeypad(int[] numbers, String hand) {
+        StringBuilder result = new StringBuilder();
+        String[][] keypad = {{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}, {"*", "0", "#"}};
+
+        int[] currentLeft = new int[] {3, 0}; // 왼손 시작점 *
+        int[] currentRight = new int[] {3, 2}; // 오른손 시작점 #
+
+        for (int idx=0; idx<numbers.length; idx++) { // [IntelliJ] 'for' loop replaceable with enhanced 'for'
+            String pressed = String.valueOf(numbers[idx]);
+//            System.out.print("> " + pressed + "\t");
+            for (int i=0; i<keypad.length; i++) {
+                for (int j=0; j<keypad[i].length; j++) {
+//                    System.out.printf("#### > [%d][%d] = %s\t", i, j, keypad[i][j]);
+//                    if (j == 2) System.out.println();
+                    if (pressed.equals(keypad[i][j])) {
+                        if (j == 0) {
+                            result.append("L");
+                            currentLeft[0] = i; currentLeft[1] = j;
+                            break;
+                        }
+                        else if (j == 2) {
+                            result.append("R");
+                            currentRight[0] = i; currentRight[1] = j;
+                            break;
+                        }
+                        else {
+                            int left = 0;
+                            int right = 0;
+                            int leftRow = Math.abs(i - currentLeft[0]);
+                            int leftColumn = Math.abs(j - currentLeft[1]);
+                            int rightRow = Math.abs(i - currentRight[0]);
+                            int rightColumn = Math.abs(j - currentRight[1]);
+
+                            if (leftRow > rightRow) left += leftRow;
+                            if (leftRow < rightRow) right += rightRow;
+                            if (leftColumn > rightColumn) left += leftColumn;
+                            if (leftColumn < rightColumn) left += rightColumn;
+
+                            if (left > right) {
+                                result.append("R");
+                                currentRight[0] = i; currentRight[1] = j;
+                            }else if (left < right) {
+                                result.append("L");
+                                currentLeft[0] = i; currentLeft[1] = j;
+                            }else {
+                                if ("right".equalsIgnoreCase(hand)) {
+                                    result.append("R");
+                                    currentRight[0] = i; currentRight[1] = j;
+                                }else {
+                                    result.append("L");
+                                    currentLeft[0] = i; currentLeft[1] = j;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("return > " + result.toString());
+        return result.toString();
+    }
+}
