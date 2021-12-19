ed50ada9de42ea9be632a09ee22f1a17304f33a0
throughhyej <hyejeyh@gmail.com>
2021-12-19 21:32:12 +0900

    [프로그래머스] 키패드 누르기

--- src/com/company/level1/Keypad.java
+++ src/com/company/level1/Keypad.java
@@ -24,11 +24,8 @@ public class Keypad {
         for (int idx=0; idx<numbers.length; idx++) { // [IntelliJ] 'for' loop replaceable with enhanced 'for'
             String pressed = String.valueOf(numbers[idx]);
             if (!pressed.matches("[0-9]")) continue;
-//            System.out.print("> " + pressed + "\t");
             for (int i=0; i<keypad.length; i++) {
                 for (int j=0; j<keypad[i].length; j++) {
-//                    System.out.printf("#### > [%d][%d] = %s\t", i, j, keypad[i][j]);
-//                    if (j == 2) System.out.println();
                     if (pressed.equals(keypad[i][j])) {
                         if (j == 0) {
                             result.append("L");
@@ -39,16 +36,14 @@ public class Keypad {
                             currentRight[0] = i; currentRight[1] = j;
                         }
                         else {
-                            int left = 0;
-                            int right = 0;
                             int leftRow = Math.abs(i - currentLeft[0]);
                             int leftColumn = Math.abs(j - currentLeft[1]);
                             int rightRow = Math.abs(i - currentRight[0]);
                             int rightColumn = Math.abs(j - currentRight[1]);
 
-                            // 이동 총 거리 체크 후 비교로 수정
-                            left = leftRow + leftColumn;
-                            right = rightRow + rightColumn;
+                            // 이동 총 거리 체크 후 비교
+                            int left = leftRow + leftColumn;
+                            int right = rightRow + rightColumn;
 
                             if (left > right) {
                                 result.append("R");
@@ -70,8 +65,8 @@ public class Keypad {
                 }
             }
         }
-        System.out.println();
-        System.out.println("return > " + result.toString());
+//        System.out.println();
+//        System.out.println("return > " + result.toString());
         return result.toString();
     }
 }
