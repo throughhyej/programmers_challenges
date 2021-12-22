c0c968270b0db61a612ac5c4e2f2e254ff85ffa4
throughhyej <hyejeyh@gmail.com>
2021-12-23 00:59:04 +0900

    [프로그래머스] 소수 만들기 진행중

--- src/com/company/level1/PrimeNumber.java
+++ src/com/company/level1/PrimeNumber.java
@@ -10,21 +10,22 @@ public class PrimeNumber {
     public static int cases = 0;
     public int getPrimeNumberCases(int[] nums) {
         // for문 수
-        cases = nums.length == 3 ? 1 : nums.length == 4 ? 4 : IntStream.range(1, nums.length-1).sum();
+        int n = nums.length;
+        cases = n == 3 ? 1 : n * (n - 3);
 
         int loop = 0;
         for (int i=0; i<cases; i++) {
-            for (int j=2; j<nums.length; j++) {
-                int result = nums[i] + nums[i+1] + nums[j];
-                System.out.printf("%d + %d + %d = ", nums[i], nums[i+1], nums[j]);
-                System.out.println("### result " + result);
-                for (int z=2; z<result; z++) {
-                    if (result % z == 0) {
-                        cases--;
-                        break;
-                    }
-                }
-            }
+//            for (int j=i+2; j<nums.length; j++) {
+//                int result = nums[i] + nums[i+1] + nums[j];
+//                System.out.printf("%d + %d + %d = ", nums[i], nums[i+1], nums[j]);
+//                System.out.println("### result " + result);
+//                for (int z=2; z<result; z++) {
+//                    if (result % z == 0) {
+//                        cases--;
+//                        break;
+//                    }
+//                }
+//            }
         }
 
         System.out.println("#### " + cases);
